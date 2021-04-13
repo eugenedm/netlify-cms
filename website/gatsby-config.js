ec2b11558abbaf034b1cbd4bd912793d3b2a0246
“Gene <way2new@gmail.com>
2021-04-14 00:13:04 +0200

    ...

--- website/gatsby-config.js
+++ website/gatsby-config.js
@@ -6,7 +6,7 @@ const staticConfig = yaml.load(fs.readFileSync('./site.yml', 'utf8'));
 
 module.exports = {
 
-  pathPrefix: `/blog`,
+  pathPrefix: ``,
 
   siteMetadata: {
     title: 'Netlify CMS | Open-Source Content Management System',
